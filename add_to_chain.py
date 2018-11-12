8190a169438745f7d758f27f775bbf5f68420127
Andrew Fowlie <andrew.j.fowlie@qq.com>
2018-11-13 11:17:50 +1300

    paralellize add to chain

--- add_to_chain.py
+++ add_to_chain.py
@@ -5,6 +5,7 @@ Add quantities to the chain
 
 import numpy as np
 from tqdm import tqdm
+from joblib import Parallel, delayed
 
 
 def add_to_chain(func, old_name, new_name=None, n_dim=4):
@@ -15,7 +16,7 @@ def add_to_chain(func, old_name, new_name=None, n_dim=4):
     """
     old_chain = np.loadtxt(old_name, unpack=True)
     params = old_chain[2:2 + n_dim]
-    add = np.array([func(*x) for x in tqdm(zip(*params))])
+    add = Parallel(n_jobs=-1)(delayed(lambda x: func(*x))(x) for x in tqdm(zip(*params)))
     new_chain = np.append(old_chain, add.T, axis=0)
 
     default_name = "{}_modified.txt".format(old_name.replace(".txt", ""))

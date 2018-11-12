c92c7b6e6bb05d4179c7614fe251c2ad27bfdeed
Andrew Fowlie <andrew.j.fowlie@qq.com>
2018-11-13 11:07:12 +1300

    watch progress

--- add_to_chain.py
+++ add_to_chain.py
@@ -4,6 +4,7 @@ Add quantities to the chain
 """
 
 import numpy as np
+from tqdm import tqdm
 
 
 def add_to_chain(func, old_name, new_name=None, n_dim=4):
@@ -14,10 +15,10 @@ def add_to_chain(func, old_name, new_name=None, n_dim=4):
     """
     old_chain = np.loadtxt(old_name, unpack=True)
     params = old_chain[2:2 + n_dim]
-    add = np.array([func(*x) for x in zip(*params)])
+    add = np.array([func(*x) for x in tqdm(zip(*params))])
     new_chain = np.append(old_chain, add.T, axis=0)
 
-    default_name = "{}_modified.txt".format(old_name.rstrip("_.txt"))
+    default_name = "{}_modified.txt".format(old_name.replace(".txt", ""))
     new_name = default_name if new_name is None else new_name
     np.savetxt(new_name, new_chain.T)
 
@@ -25,7 +26,7 @@ def add_to_chain(func, old_name, new_name=None, n_dim=4):
 if __name__ == "__main__":
 
     import sys
-    from masses import masses
+    from softsusy import masses
 
     NAME = sys.argv[1]
     add_to_chain(masses, NAME)
